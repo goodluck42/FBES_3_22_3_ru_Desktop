b07eabb30409a04a6c7c9e031c4250ad6542c0c0
xrlogic1 <xrlogic1@gmail.com>
2023-10-02 21:50:10 +0400

    *

---
+++ WinformsControls/MainForm.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace WinformsControls
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testButton = new System.Windows.Forms.Button();
+            this.xyLabel = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textboxLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.testButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.testButton.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.testButton.Location = new System.Drawing.Point(502, 12);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(268, 112);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "Click me!";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // xyLabel
+            // 
+            this.xyLabel.Location = new System.Drawing.Point(12, 9);
+            this.xyLabel.Name = "xyLabel";
+            this.xyLabel.Size = new System.Drawing.Size(484, 45);
+            this.xyLabel.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(12, 517);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 24);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "cb1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.cb_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Location = new System.Drawing.Point(244, 517);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(104, 24);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "cb3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.cb_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.Location = new System.Drawing.Point(354, 517);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(104, 24);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "cb4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.cb_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Location = new System.Drawing.Point(122, 517);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(104, 24);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "cb2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.cb_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(122, 24);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "rb1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Location = new System.Drawing.Point(6, 51);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(122, 24);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "rb2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Location = new System.Drawing.Point(6, 21);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(122, 24);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "rb3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.Location = new System.Drawing.Point(6, 51);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(122, 24);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "rb4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(10, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(216, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox1);
+            this.groupBox3.Controls.Add(this.groupBox2);
+            this.groupBox3.Location = new System.Drawing.Point(2, 236);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(497, 186);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 78);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 46);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textboxLabel
+            // 
+            this.textboxLabel.AutoSize = true;
+            this.textboxLabel.Location = new System.Drawing.Point(12, 153);
+            this.textboxLabel.MaximumSize = new System.Drawing.Size(100, 0);
+            this.textboxLabel.Name = "textboxLabel";
+            this.textboxLabel.Size = new System.Drawing.Size(0, 17);
+            this.textboxLabel.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(502, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 40);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Disable TextBox";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(168, 78);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(178, 46);
+            this.richTextBox1.TabIndex = 18;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(502, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(268, 40);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Open Color Dialog";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(502, 222);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(268, 40);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Change Color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textboxLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.xyLabel);
+            this.Controls.Add(this.testButton);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyWinformsApp";
+            this.Click += new System.EventHandler(this.MainForm_Click);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Label textboxLabel;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.GroupBox groupBox3;
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+
+        private System.Windows.Forms.CheckBox checkBox2;
+
+        private System.Windows.Forms.CheckBox checkBox1;
+
+        private System.Windows.Forms.Label xyLabel;
+
+        private System.Windows.Forms.Button testButton;
+
+        #endregion
+    }
+}
---
+++ WinformsControls/MainForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WinformsControls
+{
+    public partial class MainForm : Form
+    {
+        private System.Drawing.Color? _selectedColor = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            richTextBox1.Text = "Hello world";
+        }
+
+        private void MainForm_Click(object sender, EventArgs e)
+        {
+            return;
+            if (sender is MainForm form)
+            {
+                if (form == this)
+                {
+                    // ...
+                }
+
+                MessageBox.Show("The sender is form!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            var checkBoxes = new List<CheckBox>();
+
+            foreach (var control in Controls)
+            {
+                if (control is CheckBox c)
+                {
+                    checkBoxes.Add(c);
+                }
+            }
+
+            var rnd = new Random();
+            var randomCheckbox = checkBoxes[rnd.Next(0, checkBoxes.Count)];
+
+            randomCheckbox.Checked = !randomCheckbox.Checked;
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            xyLabel.Text = $"x: {e.X} y: {e.Y}";
+        }
+
+        private void cb_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender is CheckBox box)
+            {
+                MessageBox.Show($"{box.Text}: {box.Checked}");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textboxLabel.Text = textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Enabled)
+            {
+                textBox1.Enabled = false;
+                button1.Text = "Enable TextBox";
+            }
+            else
+            {
+                textBox1.Enabled = true;
+                button1.Text = "Disable TextBox";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var colorDialog = new ColorDialog())
+            {
+                colorDialog.ShowDialog();
+
+                _selectedColor = colorDialog.Color;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_selectedColor != null)
+            {
+                richTextBox1.SelectionColor = _selectedColor.Value;
+            }
+        }
+    }
+}
